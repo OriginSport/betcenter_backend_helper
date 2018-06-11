cf45e772d18028970536fb81bd8e3a65c0486c43
Xat_MassacrE <ywt13639397661@gmail.com>
2018-06-11 14:23:19 +0800

    Modify wsgi setting

--- betcenter_backend_helper/wsgi.py
+++ betcenter_backend_helper/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hupu_layer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betcenter_backend_helper.settings")
 
 application = get_wsgi_application()
